a63cac28d86146ee0e51ffe787f65af6e7a64b5f
thebotond <thebotond@gmail.com>
2022-01-28 18:21:23 +0000

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+
+[RequireComponent(typeof(NeuralNetwork))]
+public class CarController : MonoBehaviour
+{
+    private Vector3 startPos, startRot;
+    private NeuralNetwork network;
+    private Vector3 moveInput;
+
+    public GeneticAlgorithm geneticAlgorithm;
+
+    [Range(-1f,1f)]
+    public float a,t;
+
+    public float timeSinceStart = 0f;
+
+    [Header("Fitness values")]
+    public float overallFitness;
+    public float distanceMultipler = 1.4f;
+    public float avgSpeedMultiplier = 0.2f;
+    public float sensorMultiplier = 0.1f;
+
+    [Header("Hyperparameters")]
+    public int layers = 1;
+    public int neurons = 24;
+    public int epoch = 200;
+
+    private Vector3 lastPos;
+    private float totalDist;
+    private float avgSpeed;
+
+    [Header("Results")]
+    public int generationCount = 0;
+    public int solutionCount = 0;
+
+    private float aSensor,bSensor,cSensor;
+    private List<int> generationList = new List<int>();
+    private List<int> solutionList = new List<int>();
+    private string filePath;
+
+    private void Awake()
+    {
+        geneticAlgorithm = GameObject.Find("_GA").GetComponent<GeneticAlgorithm>();
+        startPos = transform.position;
+        startRot = transform.eulerAngles;
+        network = GetComponent<NeuralNetwork>();
+    }
+
+    public void ResetNetwork(NeuralNetwork nn)
+    {
+        network = nn;
+        Reset();
+    }
+    
+
+    public void Reset()
+    {
+        timeSinceStart = 0f;
+        totalDist = 0f;
+        avgSpeed = 0f;
+        lastPos = startPos;
+        overallFitness = 0f;
+        transform.position = startPos;
+        transform.eulerAngles = startRot;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Death();
+    }
+
+    private void FixedUpdate()
+    {
+        InputSensors();
+        lastPos = transform.position;
+
+        (a, t) = network.StartNetwork(aSensor, bSensor, cSensor);
+
+        MoveCar(a,t);
+
+        timeSinceStart += Time.deltaTime;
+        CalculateFitness();
+    }
+
+    private void Death()
+    {
+        GameObject.FindObjectOfType<GeneticAlgorithm>().Death(overallFitness, network);
+    }
+
+    private void CalculateFitness()
+    {
+        totalDist += Vector3.Distance(transform.position,lastPos);
+        avgSpeed = totalDist/timeSinceStart;
+
+       overallFitness = (totalDist*distanceMultipler)+(avgSpeed*avgSpeedMultiplier)+(((aSensor+bSensor+cSensor)/3)*sensorMultiplier);
+
+        if ((timeSinceStart > 25 && overallFitness < 50) || overallFitness > 1350)
+        {
+            if(overallFitness > 1350)
+            {
+                generationCount = geneticAlgorithm.getGenCount() + 1;
+                solutionCount++;
+                generationList.Add(generationCount);
+                solutionList.Add(solutionCount);
+            }
+            Death();
+        }
+    }
+
+    private void InputSensors()
+    {
+        Vector3 right = (transform.forward+transform.right);
+        Vector3 centre = (transform.forward);
+        Vector3 left = (transform.forward-transform.right);
+
+        Ray vision = new Ray(transform.position, right);
+        RaycastHit hit;
+
+        if (Physics.Raycast(vision, out hit))
+        {
+            aSensor = hit.distance/20;
+            Debug.DrawLine(vision.origin, hit.point, Color.red);
+        }
+
+        vision.direction = centre;
+
+        if (Physics.Raycast(vision, out hit))
+        {
+            bSensor = hit.distance/20;
+            Debug.DrawLine(vision.origin, hit.point, Color.red);
+        }
+
+        vision.direction = left;
+
+        if (Physics.Raycast(vision, out hit))
+        {
+            cSensor = hit.distance/20;
+            Debug.DrawLine(vision.origin, hit.point, Color.red);
+        }
+
+    }
+
+    public void MoveCar (float v, float h)
+    {
+        moveInput = Vector3.Lerp(Vector3.zero,new Vector3(0,0,v*11.4f),0.02f);
+        moveInput = transform.TransformDirection(moveInput);
+        transform.position += moveInput;
+
+        transform.eulerAngles += new Vector3(0, (h*90)*0.02f,0);
+    }
+
+    public void decreaseEpoch()
+    {
+        epoch--;
+    }
+
+    public void checkEpoch()
+    {
+        if(this.epoch == 0)
+        {
+            writeToFile();
+            Time.timeScale = 0;
+        }
+    }
+
+    private void writeToFile()
+    {
+        float ratio = 0; ;
+        filePath = getPath();
+        StreamWriter writer = new StreamWriter(filePath);
+        writer.WriteLine("Solution Count,Generation Count,Ratio");
+
+        for(int i = 0; i < Math.Max(solutionList.Count, generationList.Count); i++)
+        {
+            if(i < solutionList.Count)
+            {
+                writer.Write(solutionList[i]);
+            }
+            writer.Write(",");
+
+            if(i < generationList.Count)
+            {
+                writer.Write(generationList[i]);
+            }
+            writer.Write(",");
+            ratio = (float)solutionList[i] / (float)generationList[i];
+            writer.Write(ratio);
+            writer.Write(System.Environment.NewLine);
+        }
+
+        writer.Flush();
+        writer.Close();
+    }
+
+    private string getPath()
+    {
+        return Application.dataPath + "sols.csv";
+    }
+}
---
+++ GeneticAlgorithm.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra; //matrices
+
+
+public class GeneticAlgorithm : MonoBehaviour
+{
+    [Header("Refernce")]
+    public CarController controller;
+
+    [Header("Population")]
+    public int initialPopulation = 300;
+
+    [Range(0.0f, 1.0f)]
+    public float mutationRate = 0.04f;
+
+    [Header("Crossover Control")]
+    public int selectBest = 146;
+    public int selectWorst = 4;
+    public int crossoverAmount = 4;
+
+    private List<int> genePool = new List<int>();
+
+    private int selectedCount;
+    private NeuralNetwork[] population;
+
+    [Header("Population tracker")]
+    public int currentGen = 0;
+    public int currentChrom = 0;
+
+    private void Start()
+    {
+        InitialisePopulation();
+    }
+
+    private void InitialisePopulation()
+    {
+        population = new NeuralNetwork[initialPopulation];
+        RandomPopulate(population, 0);
+        ResetToCurrentGenome();
+    }
+
+    private void RandomPopulate(NeuralNetwork[] newPop, int i)
+    {
+        while (i < initialPopulation)
+        {
+            newPop[i] = new NeuralNetwork();
+            newPop[i].Initialise(controller.layers, controller.neurons);
+            i++;
+        }
+    }
+
+    private void ResetToCurrentGenome()
+    {
+        controller.ResetNetwork(population[currentChrom]);
+    }
+
+    //car controller sends fitness value and network details for evaluation on death
+    public void Death(float passedFitness, NeuralNetwork nn)
+    {
+        if (currentChrom < population.Length - 1)
+        {
+            population[currentChrom].fitness = passedFitness;
+            currentChrom++;
+            ResetToCurrentGenome();
+        }
+        else
+        {
+            Repopulate();
+        }
+    }
+
+    private void Repopulate()
+    {
+        controller.decreaseEpoch();
+        controller.checkEpoch();
+        genePool.Clear();
+        currentGen++;
+        selectedCount = 0;
+        SortPopulation();
+        NeuralNetwork[] newPop = PickBest();
+
+        Crossover(newPop);
+        Mutation(newPop);
+
+        RandomPopulate(newPop, selectedCount);
+
+        currentChrom = 0;
+        ResetToCurrentGenome();
+    }
+
+    public void Crossover(NeuralNetwork[] newPop)
+    {
+        for (int i = 0; i < crossoverAmount; i += 2)
+        {
+            int point1 = i;
+            int point2 = i + 1;
+            if (genePool.Count > 0)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    point1 = genePool[Random.Range(0, genePool.Count)];
+                    point2 = genePool[Random.Range(0, genePool.Count)];
+
+                    if (point1 != point2)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            NeuralNetwork offspring1 = new NeuralNetwork();
+            NeuralNetwork offspring2 = new NeuralNetwork();
+            offspring1.Initialise(controller.layers, controller.neurons);
+            offspring2.Initialise(controller.layers, controller.neurons);
+            offspring1.fitness = 0;
+            offspring2.fitness = 0;
+
+            for (int k = 0; k < offspring1.weights.Count; k++)
+            {
+                if (Random.Range(0.0f, 1.0f) < 0.5f)
+                {
+                    offspring1.weights[k] = population[point1].weights[k];
+                    offspring2.weights[k] = population[point2].weights[k];
+                }
+                else
+                {
+                    offspring1.weights[k] = population[point2].weights[k];
+                    offspring2.weights[k] = population[point1].weights[k];
+                }
+            }
+
+            for (int l = 0; l < offspring1.biases.Count; l++)
+            {
+                if (Random.Range(0.0f, 1.0f) < 0.5f)
+                {
+                    offspring1.biases[l] = population[point1].biases[l];
+                    offspring2.biases[l] = population[point2].biases[l];
+                }
+                else
+                {
+                    offspring1.biases[l] = population[point2].biases[l];
+                    offspring2.biases[l] = population[point1].biases[l];
+                }
+            }
+
+            newPop[selectedCount] = offspring1;
+            selectedCount++;
+            newPop[selectedCount] = offspring2;
+            selectedCount++;
+        }
+    }
+
+    public void Mutation(NeuralNetwork[] newPop)
+    {
+        for (int i = 0; i < selectedCount; i++)
+        {
+            for (int j = 0; j < newPop[i].weights.Count; j++)
+            {
+                if (Random.Range(0.0f, 1.0f) < mutationRate)
+                {
+                    newPop[i].weights[j] = MutateMatrix(newPop[i].weights[j]);
+                }
+            }
+        }
+    }
+    Matrix<float> MutateMatrix(Matrix<float> A)
+    {
+        int randomPoints = Random.Range(1, (A.RowCount * A.ColumnCount) / 7);
+        Matrix<float> B = A;
+
+        for (int i = 0; i < randomPoints; i++)
+        {
+            int randomColumn = Random.Range(0, B.ColumnCount);
+            int randomRow = Random.Range(0, B.RowCount);
+            B[randomRow, randomColumn] = Mathf.Clamp(B[randomRow, randomColumn] + Random.Range(-1f, 1f), -1f, 1f);
+        }
+
+        return B;
+    }
+
+    private void SortPopulation()
+    {
+        for (int i = 0; i < population.Length; i++)
+        {
+            for (int j = 0; j < population.Length; j++)
+            {
+                if (population[i].fitness < population[j].fitness)
+                {
+                    NeuralNetwork temp = population[i];
+                    population[i] = population[j];
+                    population[j] = temp;
+                }
+            }
+        }
+    }
+
+    private NeuralNetwork[] PickBest()
+    {
+        NeuralNetwork[] newPop = new NeuralNetwork[initialPopulation];
+
+        for (int i = 0; i < selectBest; i++)
+        {
+            newPop[selectedCount] = population[i].InitialiseCopy(controller.layers, controller.neurons);
+            newPop[selectedCount].fitness = 0;
+            selectedCount++;
+
+            int f = Mathf.RoundToInt(population[i].fitness * 10);
+            for (int x = 0; x < f; x++)
+            {
+                genePool.Add(i);
+            }
+        }
+
+        for (int j = 0; j < selectWorst; j++)
+        {
+            int last = population.Length - 1;
+            last -= j;
+
+            int g = Mathf.RoundToInt(population[last].fitness * 10);
+            for (int y = 0; y < g; y++)
+            {
+                genePool.Add(last);
+            }
+        }
+        return newPop;
+    }
+
+    public int getChromCount()
+    {
+        return currentChrom;
+    }
+
+    public int getGenCount()
+    {
+        return currentGen;
+    }
+}
